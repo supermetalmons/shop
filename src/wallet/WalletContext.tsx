99933c4eb91100fffc1e560766a2565c6d2830c4
ivan grachev <ivan@ivan.lol>
2025-12-24 20:57:52 +0300

    readd glow adapter

--- src/wallet/WalletContext.tsx
+++ src/wallet/WalletContext.tsx
@@ -9,6 +9,7 @@ import {
   LedgerWalletAdapter,
   PhantomWalletAdapter,
 } from '@solana/wallet-adapter-wallets';
+import { GlowWalletAdapter } from '@solana/wallet-adapter-glow';
 import { clusterApiUrl } from '@solana/web3.js';
 import '@solana/wallet-adapter-react-ui/styles.css';
 import { getHeliusApiKey } from '../lib/helius';
@@ -41,6 +42,7 @@ export const WalletContextProvider: FC<Props> = ({ children }) => {
     () => [
       new PhantomWalletAdapter(),
       new LedgerWalletAdapter(),
+      new GlowWalletAdapter({ network }),
     ],
     [],
   );
