9876d27075728e005d3eb97d82f4cc83cf11b409
ivan grachev <ivan@ivan.lol>
2025-12-24 18:59:42 +0300

    tune title

--- src/App.tsx
+++ src/App.tsx
@@ -562,7 +562,10 @@ function App() {
     <div className="page">
       <header className="top">
         <div className="brand">
-          <h1><img src="https://assets.mons.link/shop/logo.webp" alt="" className="brand-icon" />mons.shop</h1>
+          <h1>
+            <img src="https://assets.mons.link/shop/logo.webp" alt="" className="brand-icon" />
+            <span>mons.shop</span>
+          </h1>
         </div>
         <WalletMultiButton />
       </header>
