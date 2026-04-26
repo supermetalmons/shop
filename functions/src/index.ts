fa7a042472e03cc12b4d6ad44351a0e9c83c86d0
ivan grachev <ivan@ivan.lol>
2026-04-26 18:44:05 +0300

    clean up notif drops

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -433,7 +433,7 @@ const SHIPPER_FULFILLMENT_ACCESS: ShipperFulfillmentAccessConfig[] = [
 
 const SHIPPER_READY_TO_SHIP_NOTIFICATIONS: ShipperReadyToShipNotificationConfig[] = [
   {
-    dropIds: ['little_swag_boxes', 'poncho_drifella', 'little_swag_hoodies', 'little_swag_hoodies_devnet'],
+    dropIds: ['little_swag_boxes', 'poncho_drifella', 'little_swag_hoodies'],
     emails: ['ivan@ivan.lol', 'supermetalxbosch@gmail.com'],
   },
 ];
