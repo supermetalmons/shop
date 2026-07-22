1ae196134616bf5ae3565ba210cb2215d1cb2dfb
ivan grachev <ivan@ivan.lol>
2026-07-22 20:58:22 +0300

    deploy devnet shirt

--- onchain/programs/box_minter/src/lib.rs
+++ onchain/programs/box_minter/src/lib.rs
@@ -5,7 +5,7 @@ use anchor_lang::solana_program::program::invoke_signed;
 use core::fmt::Write;
 use solana_sha256_hasher::hashv;
 
-declare_id!("7h4JRc5vELpaahm11AeshFEQHe1jePauRnMFWaPSRNpV");
+declare_id!("Hr39xMTdeQFPkLb9D6yYxxzTTkfW6QgVyyUETT7jyfZw");
 
 /// The only signer allowed to run `initialize()`.
 ///
